470f73fc387a7afcc598e0a519831b34059f5e78
Moonlightmoreno <morenovp36@gmail.com>
2026-05-25 21:57:57 +0200

    Some Hover effects. I created also a german Version.

--- game.js
+++ game.js
@@ -1,21 +1,22 @@
-//words from https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt
+//words in english from https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt
+//words in german from https://gist.githubusercontent.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4/raw/36b70dd6be330aa61cd4d4cdfda6234dcb0b8784/wordlist-german.txt
 //emojis for js https://unicode.org/emoji/charts/full-emoji-list.html#1f600
 let curRow = 1;
 let win = false;
 var date = new Date().getDate();
 function start(){
-    loadWord(Math.floor(Math.random() * 3104));
+    loadWord();
     const input = document.getElementById("inp");
     input.focus(    );
     document.addEventListener('click',function(event){
         input.focus(    );
     });
     document.addEventListener('keydown',function(event){
-        if (event.key==='Enter' && document.getElementById("inp").value.length == 5 && !win && ArrWord.includes(document.getElementById("inp").value)){
+        if (event.key==='Enter' && document.getElementById("inp").value.length == 5 && !win && ArrWord.includes(document.getElementById("inp").value.toLowerCase())){
             //alert(word==document.getElementById("inp").value);
 
             let real = word.split('');
-            let given = String(document.getElementById("inp").value).split('');
+            let given = String(document.getElementById("inp").value.toLowerCase()).split('');
             //alert(real); 
             //alert(given);
 
@@ -34,7 +35,7 @@ function start(){
                 }
             }
 
-            if (word==document.getElementById("inp").value){
+            if (word==document.getElementById("inp").value.toLowerCase()){
                 win = true;
                 document.getElementById("overnew").style.display = "block";
                 document.getElementById("New Game").textContent = "You win! The word was " + word + " " + String.fromCodePoint(0x1F973);
@@ -57,7 +58,7 @@ function start(){
             }
             //alert(document.getElementById("inp").value);
             document.getElementById("inp").value = null;
-        } else if (event.key==='Enter' && document.getElementById("inp").value.length == 5 && !ArrWord.includes(document.getElementById("inp").value)){
+        } else if (event.key==='Enter' && document.getElementById("inp").value.length == 5 && !ArrWord.includes(document.getElementById("inp").value.toLowerCase())){
             const dist = document.getElementById("overtext");
             dist.classList.remove('invalidanimation');
             
@@ -70,22 +71,34 @@ function start(){
     });
 }
 function adjustFields(){
-    let input = document.getElementById("inp").value.split('');
+    let input = document.getElementById("inp").value.toLowerCase().split('');
     for (let i  = 1; i<6; i++){
         document.getElementById(String.fromCharCode(96 + curRow) + i).textContent = input[i-1];
     }
 }
-async function loadWord(RandNum) {
-    const res = await fetch('words.json');
-    const data = await res.json();
+async function loadWord() {
+    const params = new URLSearchParams(window.location.search);
+    const language = params.get('language'); 
+    let data;
+    let lendata;
+    if (language === 'german') {
+        const res = await fetch('words_german.json');
+        data = await res.json();
+        lendata = 4539;
+    } 
+    if (language === 'english') {
+        const res = await fetch('words.json');
+        data = await res.json();
+        lendata = 5708;
+    }
     Object.defineProperty(window, 'ArrWord', {
-        value: data.slice(0,5757),
+        value: data.slice(0,lendata),
         writable: false,
         configurable: false
     });
     //alert(grr[RandNum]);
     Object.defineProperty(window, 'word', {
-        value: ArrWord[RandNum],
+        value: ArrWord[Math.floor(Math.random() * lendata)],
         writable: false,
         configurable: false
     });
--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-let stG = false;
-function startBlock() {
-    if (stG==false){
-        document.getElementById("stGame").innerHTML = "No";
-        document.getElementById("GameB").innerHTML = "Try again";
-        stG = true;
-    }else{
-        window.location.href = "game.html";
-    }
+function Wordlebtn() {
+    document.getElementById("title").innerHTML = "Select Game Mode";
+    document.getElementById("WordleBtn").innerHTML = "Wordle";
+    document.getElementById("WordleGerBtn").style.visibility = "visible";
+    document.getElementById("MathleBtn").style.visibility = "visible";
+    document.getElementById("WordleBtn").addEventListener("click", function(event) {
+        window.location.href = "game.html?language=english";
+    });
+}
+function WordleGerbtn() {
+    window.location.href = "game.html?language=german";
+}
+function Mathlebtn() {
+    window.location.href = "game.html";
 }
